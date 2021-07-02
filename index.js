83e85aad2d5f9c70b61d52e5f9a444302dcfe8db
jrrohrer <jessie.rohrer@gmail.com>
2021-07-02 15:56:03 -0400

    commit tree 1df7f4574d3e1a91abac0eb6b796cd8a1eb69c4e

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Add your code here
+function submitData(username, email) {
+  let formData = {
+    name: username,
+    email: email
+  }
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message;
+    })
+}
+
+/*
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarok!");
+    console.log(error.message);
+  });
+
+*/
